d35918dc879deffad659b0363e66833ae9865dde
Burak-Cengiz <66324290+Burak-Cengiz@users.noreply.github.com>
2022-05-26 16:20:22 +0300

    C# ile Stack Veri Yapisi
    Stack Veri Yapisi gerceklestirimi

---
+++ Stack/Stack/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stack
+{
+    public partial class Stack : Form
+    {
+        public Stack()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = 2;
+            if(x == 2 )
+            {
+                MessageBox.Show("anan");
+            }
+            if(x == 4)
+            {
+                MessageBox.Show("baban");
+            }
+            else
+            {
+                MessageBox.Show("gf");
+            }
+           
+        }
+    }
+}
